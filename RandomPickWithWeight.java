828ea6f922ea8d9a0f25f406ae69f1b94917bf42
Samar Eqbal <samareqbal17@gmail.com>
2020-06-06 12:26:50 +0530

    fifth solution

---
+++ RandomPickWithWeight.java
@@ -0,0 +1,39 @@
+package LeetCodeJune;
+
+import java.util.Random;
+
+public class RandomPickWithWeight {
+    int[] accumulatedSum;
+     Random random;
+     
+    public Solution(int[] w) {
+        random = new Random();
+        accumulatedSum = new int[w.length];
+        int currSum = 0;
+        for (int i = 0; i < w.length; i++) {
+            currSum += w[i];
+            accumulatedSum[i] = currSum;
+        }
+    }
+    
+    public int pickIndex() {
+         int target = random.nextInt(accumulatedSum[accumulatedSum.length - 1]) + 1;
+        return binarySearch(accumulatedSum, 0, accumulatedSum.length - 1, target);
+    }
+     private int binarySearch(int[] arr, int left, int right, int target) {
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (arr[mid] > target) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+}
