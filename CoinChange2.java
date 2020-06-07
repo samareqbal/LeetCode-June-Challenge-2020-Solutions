3bcbc0c4fec7a1a13aa432115f18d81685cdaa15
Samar Eqbal <samareqbal17@gmail.com>
2020-06-07 19:26:18 +0530

    seventh sol added

---
+++ CoinChange2.java
@@ -0,0 +1,13 @@
+package LeetCodeJune;
+public class CoinChange2 {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+}
