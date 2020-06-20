94d1d7f8d6da38155bbe3901eb66b4af3a5148a1
Samar Eqbal <samareqbal17@gmail.com>
2020-06-21 02:28:12 +0530

    twentieth solution

---
+++ PermutationSequence.java
@@ -0,0 +1,28 @@
+package LeetCodeJune;
+
+import java.util.*;
+
+public class PermutationSequence {
+    public String getPermutation(int n, int k) {
+        StringBuilder sb = new StringBuilder("");
+	List<Integer> num = new ArrayList<Integer>();
+	for(int i = 1; i <= n; i++) // save candidate elements in a list
+		num.add(i);
+	int factorial = 1;
+	for(int i = 1; i <= n - 1; i++)
+		factorial *= i;
+	k = k - 1; // synchronize the index
+	for(int i = 1; i < n; i++) {
+		if( k == 0 )
+			break;
+		int index = k / factorial;
+		k %= factorial;
+		factorial /= (n - i);
+		sb.append(num.get(index));
+		num.remove(index);
+	}
+	for(int i = 0; i < num.size(); i++)
+		sb.append(num.get(i));
+	return sb.toString();
+    }
+}
